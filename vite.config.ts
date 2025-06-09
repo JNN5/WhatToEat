dc681430c6b003fbc89a5971eba0dcfdd27f63de
Jonas Neumann <jonas.a.neumann@accenture.com>
2025-06-09 18:42:11 +0800

    adds gh pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/WhatToEat/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
